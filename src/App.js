dbdf4ad3363be9b83a40260fcb31464b2f2f0878
Yuki Zhang <yukizhang@wirelessprv-10-194-172-138.near.illinois.edu>
2022-04-14 21:24:32 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 import Dropdown from './Dropdown';
 
 function App() {
+  const [open, setOpen] = useState(false); /* dropdown open/closed */
   const options = ['first', 'second', 'third', 'fourth', 1, 2, 3, 4, 5];
- 
+
+  function handleToggle(){
+    setOpen(!open)
+  }
+  function handleClose(e){
+    // if the form is open and the click is not on the dropdown list, close the dropdown
+    if (open && (e.target.getAttribute("option") === null)) {setOpen(false)};
+  }
   return (
-    <div className="app">
+    <div className="app" onClick={handleClose}>
       <div className="app__outer">
-      <Dropdown options={options}/>
+      <Dropdown options={options} open={open} setOpen={setOpen} handleToggle={handleToggle}/>
       </div>
     </div>
   );
--- src/Dropdown.js
+++ src/Dropdown.js
@@ -5,38 +5,56 @@ import {
 import React, { useState } from "react";
 import "./dropdown.css";
 
-function Dropdown({ options }) {
-  const [open, setOpen] = useState(false);
-  const [selected, setSelected] = useState([]);
-  function handleToggle() {
-    setOpen(!open);
-  }
+const colors = ['white', '#CECECE']
 
+function Dropdown({ options, open, setOpen, handleToggle }) {
+//   use a number to keep track of selected items, e.g. 1101 means the first, third and fourth
+// items are selected
+  const [currSelection, setCurrSelection]  = useState(0);
+  const [selected, setSelected] = useState([]); /* list of selected options*/
+
+
+// compute color of an item
+  function computeColor(index){
+      if ((currSelection & (1 << index)) > 0){
+          return colors[1];
+      } else{
+          return colors[0];
+      }
+  }
+//   toggle color of selected/unselected item
+  function toggleSelection(index){
+      setCurrSelection(currSelection ^ (1 << index))
+  }
+  
   function handleSelect(e) {
+    // fill the displayed list of selected items
     const option = e.target.getAttribute("option");
-    console.log(selected.indexOf(option));
     if (selected.indexOf(option) >= 0) {
       const ind = selected.indexOf(option);
       setSelected(selected.filter((val, i) => i !== ind))
     } else {
       setSelected([...selected, option]);
-      console.log(2);
     }
-    console.log(selected);
+
+    // toggle color of selected/unselected item
+    const index = e.target.getAttribute("ind");
+    toggleSelection(index);
+
   }
   return (
     <div className="dropdown">
       <button className="dropdown__btn" onClick={handleToggle}>
-        <p className="dropdown__text">{selected.join(",")}</p>
+        <p className="dropdown__text">{selected.join(", ")}</p>
         {!open ? 
         <ArrowDropDown className="dropdown__arrow"/> :
         <ArrowDropUp className="dropdown__arrow"/>}
       </button>
       {open && (
         <div className="dropdown__content">
-          {options.map((option) => {
+          {options.map((option, i) => {
             return (
-              <div className="dropdown__option" option={option} onClick={handleSelect} key={option}>
+              <div className="dropdown__option" style={{backgroundColor: computeColor(i)}} option={option} ind={i} onClick={handleSelect} key={option}>
                 {option}
               </div>
             );
